18e7372b6fcdc5b2401f3553e09f4de03b70c3dc
Andrey-XP <andrey.sousalipe@gmail.com>
2025-12-05 12:30:37 -0300

    Update Alunos.java

--- gerenciadordealunos/Alunos.java
+++ gerenciadordealunos/Alunos.java
@@ -3,5 +3,10 @@ package gerenciador_de_alunos;
 class Alunos{
     public static void main(String[] args) {
         
+        for(int i = 0; i < agendamentos.size(); i++){
+            Agendamento ag = agendamentos.get(i);
+            System.out.println("Cliente: " + ag.getNome());
+            System.out.println("Horario: " + ag.getHorario());
+        }
     }
 }
